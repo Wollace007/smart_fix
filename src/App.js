4cc5e25f5836a102dfe3d0bca25471eec75b6eed
jobkinyua2 <jobkinyua21@example.com>
2023-04-07 10:28:22 +0300

    change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
        <Route path="/home"element={<Home/>}/>
        <Route path="/test"element={<Intropage/>}/>
       <Route path="/homecar"element={<Homecarousel/>}/>
-      <Route path="/homecar"element={<Homecarousel/>}/>
       <Route path="/multi"element={<MultiCarousel/>}/>
       
         </Routes>
