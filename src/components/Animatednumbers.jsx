961a8ec2ef230c6a94122d61611d27b79ae594ad
Wollace <wollacemaina2@gmail.com>
2023-04-14 12:22:15 +0300

    smart_fix

--- src/components/Animatednumbers.jsx
+++ src/components/Animatednumbers.jsx
@@ -1,9 +1,37 @@
+import { Add } from '@mui/icons-material';
+import { Box, Divider, Grid, Typography } from '@mui/material';
 import React from 'react'
-
+import CountUp from 'react-countup';
 function Animatednumbers() {
   return (
     <>
-      helllo
+ <Grid container spacing={2} style={{display:"flex",justifyContent:"center"}}>
+    <Grid item xs={6}>
+    <CountUp start={0} end={2456} delay={1}>
+  {({ countUpRef }) => (
+    <Box style={{flexDirection:"column"}}>
+      <span ref={countUpRef} style={{color:"#fc8f00",fontSize:"2rem",fontWeight:"bolder"}}>
+      </span>
+      <Add style={{color:"#fff",fontSize:"2rem"}}/>
+      <Typography style={{color:"#fff",fontSize:"1.2rem",display:"flex",justifyContent:"flex-start"}}> Clients Satisfied With Our Services</Typography>
+    </Box>
+  )}
+</CountUp>
+    </Grid>
+    <Divider style={{border:"#fff"}} />
+    <Grid item xs={6}>
+    <CountUp start={0} end={3214} delay={1}>
+  {({ countUpRef }) => (
+    <Box style={{flexDirection:"column"}}>
+      <span ref={countUpRef} style={{color:"#fc8f00",fontSize:"2rem",fontWeight:"bolder"}}> </span> 
+      <Add style={{color:"#fff",fontSize:"2rem"}}/>
+      <Typography style={{color:"#fff",fontSize:"1.2rem",display:"flex",justifyContent:"flex-start"}}> Project Completed By Our Service</Typography>
+    </Box>
+  )}
+</CountUp>
+    </Grid>
+  </Grid>
+
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Add, ChevronRight, Phone } from '@mui/icons-material';
+import { Add, Bolt, ChevronRight, ContentPasteSearch, Cottage, EditCalendar, Engineering, MilitaryTech, Phone, PhoneInTalk } from '@mui/icons-material';
 import { Box, Button, Card, Divider, Grid, Paper, TextField, Typography } from '@mui/material'
 import React from 'react'
 import Navbar from '../components/Navbar'
@@ -7,8 +7,12 @@ import Typewriter from 'typewriter-effect';
 import { Link } from 'react-router-dom';
 import handymanimage from '../media/images/happy-young-man.jpg'
 import repairtools from '../media/images/top-view-mechanical-tools-arrangement.jpg'
+import Wark from '../media/images/work.jpg'
 import Homecarousel from '../components/homecarousel/Homecarousel';
 import Animatednumbers from '../components/Animatednumbers';
+import Wrench from '../media/images/wrench.jpg';
+import electrician from '../media/images/repairingelectrical.jpg';
+import doorrepair from '../media/images/installeddoorlocker.jpg';
 function Home() {
 
   return (
@@ -155,16 +159,278 @@ function Home() {
             </Box>
           </Grid>
           <Grid item xs={6}>
-            <Card sx={{ backgroundColor: "#08011D" }}>
-              <Box> <Animatednumbers/> </Box>
-              <Box> step 2 </Box>
-              <Divider />
-              <Box> step 1 </Box>
-              <Box> step 2 </Box>
+            <Card sx={{ flexGrow: 1, padding: "4rem", backgroundColor: "#08011D", marginTop: "2rem" }}>
+              <Animatednumbers />
             </Card>
           </Grid>
         </Grid>
       </Box>
+      <Box style={{ flexGrow: 1, backgroundColor: "#F2F2F8" }}>
+
+        <Grid item xs={12} style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+          <span style={{ fontWeight: "bold", fontSize: "1.5rem", color: "#fc8f00" }}>How it works</span>
+          <span style={{ fontWeight: "bold", fontSize: "2.4rem" }}> Follow 4 Easy Steps</span>
+        </Grid>
+      </Box>
+      <Grid container spacing={2} style={{ flexGrow: 1, padding: "3rem", display: "flex", justifyContent: "center", backgroundColor: "#F2F2F8" }}>
+        <Grid item xs={2}>
+          <div style={{ display: "flex", alignItems: "center", marginLeft: "4rem" }}>
+            <img
+              style={{ width: "50px", height: "50px" }}
+              src="https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-no-1.png" />
+          </div>
+          <div className='steps' style={{
+            borderStyle: "solid", borderWidth: "2px 2px 2px 2px",
+            borderRadius: "5px 5px 5px 5px", borderColor: "#fc8f00", padding: "2rem"
+          }}>
+            <span className='stepsIcon' style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <EditCalendar style={{ fontSize: "50px" }} />
+            </span>
+          </div>
+          <span className='steps'> Schedule your Experience</span>
+          <Typography style={{ color: '#000000', textAlign: "center" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Typography>
+        </Grid>
+        <Box style={{ display: "flex", justifyContent: "center", marginTop: "5rem" }}>
+          <img style={{ width: "100px", height: "100px" }}
+            src="https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-arrow-1-e1616025617515.png" />
+        </Box>
+        <Grid item xs={2}>
+          <div style={{ display: "flex", alignItems: "center", marginLeft: "4rem" }}>
+            <img
+              style={{ width: "50px", height: "50px" }}
+              src="https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-no-2.png" />
+          </div>
+          <div className='steps' style={{
+            borderStyle: "solid", borderWidth: "2px 2px 2px 2px",
+            borderRadius: "5px 5px 5px 5px", borderColor: "#fc8f00", padding: "2rem"
+          }}>
+            <span className='stepsIcon' style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <ContentPasteSearch style={{ fontSize: "50px" }} />
+            </span>
+          </div>
+          <span className='steps'> Get Professional Advices</span>
+          <Typography style={{ color: '#000000', textAlign: "center" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Typography>
+        </Grid>
+        <Box style={{ display: "flex", justifyContent: "center", marginTop: "5rem" }}>
+          <img style={{ width: "100px", height: "100px" }}
+            src="	https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-arrow-1-e1616025617515.png" />
+        </Box>
+        <Grid item xs={2}>
+          <div style={{ display: "flex", alignItems: "center", marginLeft: "4rem" }}>
+            <img
+              style={{ width: "50px", height: "50px" }}
+              src="https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-no-3.png" />
+          </div>
+          <div className='steps' style={{
+            borderStyle: "solid", borderWidth: "2px 2px 2px 2px",
+            borderRadius: "5px 5px 5px 5px", borderColor: "#fc8f00", padding: "2rem"
+          }}>
+            <span className='stepsIcon' style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <Engineering style={{ fontSize: "50px" }} />
+            </span>
+          </div>
+          <span className='steps'> Meet Handyman Expert</span>
+          <Typography style={{ color: '#000000', textAlign: "center" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Typography>
+        </Grid>
+        <Box style={{ display: "flex", justifyContent: "center", marginTop: "5rem" }}>
+          <img style={{ width: "100px", height: "100px" }}
+            src="	https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-arrow-1-e1616025617515.png" />
+        </Box>
+        <Grid item xs={2}>
+          <div style={{ display: "flex", alignItems: "center", marginLeft: "4rem" }}>
+            <img
+              style={{ width: "50px", height: "50px" }}
+              src="https://templatekit.jegtheme.com/fixn/wp-content/uploads/sites/55/2021/03/fixn-no-4.png" />
+          </div>
+          <div className='steps' style={{
+            borderStyle: "solid", borderWidth: "2px 2px 2px 2px",
+            borderRadius: "5px 5px 5px 5px", borderColor: "#fc8f00", padding: "2rem"
+          }}>
+            <span className='stepsIcon' style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <MilitaryTech style={{ fontSize: "50px" }} />
+            </span>
+          </div>
+          <span className='steps'> Get A Best Our Services</span>
+          <Typography style={{ color: '#000000', textAlign: "center" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card style={{
+            display: "flex", justifyContent: "center",
+            backgroundImage: `url(${Wark})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: "100vh"
+          }}>
+            <Box style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
+              <span style={{ color: "#FFA400", display: "flex", justifyContent: "center", fontSize: "1.5rem", fontWeight: "bolder" }}>
+                Interested In An Estimate?
+              </span>
+              <Typography variant="h3" component="h2" style={{ color: "#FFFFFF", display: "flex", justifyContent: "center", fontWeight: "bold" }}>
+                Get Started With Your Free Estimate
+              </Typography>
+              <span style={{ color: "#ffffff", textAlign: "center", fontWeight: "bold" }}>
+                Donec feugiat lectus mi, nec scelerisque massa viverra ac. Vivamus vestibulum ornare imperdiet.
+                Nam et sagittis diam. Maecenas maximus fringilla viverra.
+              </span>
+              <Box style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}>
+                <Button variant="contained" size="small" style={{ backgroundColor: "#fc8f00", padding: "12px 100px" }}>
+                  REQUEST ESTIMATE
+                </Button>
+              </Box>
+            </Box>
+          </Card>
+          <Paper elevation={0} style={{ flexGrow: 1 }}>
+            <Grid container spacing={2} style={{ padding: "2rem" }}>
+              <Grid item xs={4}>
+                <div style={{
+                  backgroundImage: `url(${Wrench})`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  height: "35vh"
+                }}>
+                  <Box style={{ padding: "2rem" }}>
+                    <span style={{ color: "#fc8f00", fontWeight: "bold", fontSize: "1rem" }}>Special Offer</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem", display: "flex" }}>  Faucet Installation</span>
+                    <span className='specialOffer'>$24</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem" }}>off</span>
+                    <Link
+                      className='linq'
+                      href="/"
+                      target="_blank"
+                      variant="body2"
+                    >
+                      <span style={{ display: "flex", justifyContent: "flex-start", fontWeight: "bold", fontSize: "1rem" }}>Contact Us<ChevronRight /> </span>
+                    </Link>
+
+                  </Box>
+                </div>
+              </Grid>
+              <Grid item xs={4}>
+                <div style={{
+                  backgroundImage: `url(${electrician})`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  height: "35vh"
+                }}>
+                  <Box style={{ padding: "2rem" }}>
+                    <span style={{ color: "#fc8f00", fontWeight: "bold", fontSize: "1rem" }}>Special Offer</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem", display: "flex" }}>  Electrical Repair</span>
+                    <span className='specialOffer'>$24</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem" }}>off</span>
+                    <Link
+                      className='linq'
+                      href="/"
+                      target="_blank"
+                      variant="body2"
+                    >
+                      <span style={{ display: "flex", justifyContent: "flex-start", fontWeight: "bold", fontSize: "1rem" }}>Contact Us<ChevronRight /> </span>
+                    </Link>
+
+                  </Box>
+                </div>
+              </Grid>
+              <Grid item xs={4}>
+                <div style={{
+                  backgroundImage: `url(${doorrepair})`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  height: "35vh"
+                }}>
+                  <Box style={{ padding: "2rem" }}>
+                    <span style={{ color: "#fc8f00", fontWeight: "bold", fontSize: "1rem" }}>Special Offer</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem", display: "flex" }}>  Renovation Service</span>
+                    <span className='specialOffer'>$24</span>
+                    <span style={{ fontWeight: "bold", fontSize: "2rem" }}>off</span>
+                    <Link
+                      className='linq'
+                      href="/"
+                      target="_blank"
+                      variant="body2"
+                    >
+                      <span style={{ display: "flex", justifyContent: "flex-start", fontWeight: "bold", fontSize: "1rem" }}>Contact Us<ChevronRight /> </span>
+                    </Link>
+
+                  </Box>
+                </div>
+              </Grid>
+
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={4} >
+          <Box style={{ padding: "2rem", display: "flex", flexDirection: "column" }}>
+            <span style={{ color: "#fc8f00", fontSize: "1.3rem" }}>What We Offer</span>
+            <span style={{ fontWeight: "bold", fontSize: "2rem" }}>Available for 24/7 Emergency Service</span>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec vel dignissim lectus. Pellentesque et scelerisque.
+              Sedvel ipsum auctor, iaculis arcu quis, posuere dui.
+            </span>
+          </Box>
+        </Grid>
+        <Grid item xs={8} style={{ display: "flex", justifyContent: "center" }}>
+          <Grid item xs={6}>
+            <Box style={{display:"flex"}}>
+              <Box style={{display:"flex"}}>
+              <span className='stepsIcon'> <Bolt fontSize="large" fontWeight="bolder"/> </span>
+              </Box>
+              <Box style={{display:"flex",flexDirection:"column",marginLeft:"10px"}}>
+              <span className='offer' style={{fontWeight:"bold",fontSize:"1.4rem"}}> Fast Reliable Service</span>
+              <span>Lorem ipsum dolor sit amet consec tetur adipiscing elit sed eiusmod tempor incididunt.</span>
+              </Box>
+            </Box>
+
+            <Box style={{marginTop:"3rem"}}>
+            <Box style={{display:"flex",justifyContent:"flex-start"}}>
+              <Box style={{display:"flex"}}>
+              <span   className='stepsIcon'> <PhoneInTalk fontSize="large" fontWeight="bolder"/> </span>
+              </Box>
+              <Box style={{display:"flex",flexDirection:"column",marginLeft:"10px"}}>
+              <span className='offer' style={{fontWeight:"bold",fontSize:"1.4rem"}}> Emergency Service</span>
+              <span>Lorem ipsum dolor sit amet consec tetur adipiscing elit sed eiusmod tempor incididunt.</span>
+              </Box>
+            </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box>
+            <Box style={{display:"flex",justifyContent:"flex-start"}}>
+              <Box style={{display:"flex"}}>
+              <span   className='stepsIcon'> <Engineering fontSize="large" fontWeight="bolder"/> </span>
+              </Box>
+              <Box style={{display:"flex",flexDirection:"column",marginLeft:"10px"}}>
+              <span className='offer' style={{fontWeight:"bold",fontSize:"1.4rem"}}> Skill & Trained Personel</span>
+              <span>Lorem ipsum dolor sit amet consec tetur adipiscing elit sed eiusmod tempor incididunt.</span>
+              </Box>
+            </Box>
+              </Box>
+            <Box style={{marginTop:"3rem"}}>
+            <Box style={{display:"flex",justifyContent:"flex-start"}}>
+              <Box style={{display:"flex"}}>
+              <span   className='stepsIcon'> <Cottage fontSize="large" fontWeight="bolder"/> </span>
+              </Box>
+              <Box style={{display:"flex",flexDirection:"column",marginLeft:"10px"}}>
+              <span className='offer' style={{fontWeight:"bold",fontSize:"1.4rem"}}> 1 Year Warranty</span>
+              <span>Lorem ipsum dolor sit amet consec tetur adipiscing elit sed eiusmod tempor incididunt.</span>
+              </Box>
+            </Box>
+              </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+  <Grid item xs={6}>
+    step 1
+  </Grid>
+  <Grid item xs={6}>
+   step 2
+  </Grid>
+</Grid>
     </>
   )
 }
